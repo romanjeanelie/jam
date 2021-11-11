afba6cd02c975bdb69aa4dcd7eb4b0d7fcaf1564
roman7501 <64649303+roman7501@users.noreply.github.com>
2021-11-11 16:00:49 +0100

    add redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -30,6 +30,15 @@ export const getStaticProps = async ({ params }) => {
     "fields.slug": params.slug,
   });
 
+  if (!items.length) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
       recipe: items[0],
